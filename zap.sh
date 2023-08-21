afbcc108f090a5484542f48980bbc84abfd02b53
Lokesh B <lokesh.b.2020.ad@ritchennai.edu.in>
2023-08-21 14:52:40 +0530

    Changed #21

--- zap.sh
+++ zap.sh
@@ -19,7 +19,6 @@ echo "Exit Code : $exit_code"
 
  if [[ ${exit_code} -ne 0 ]];  then
     echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
-    exit 1;
    else
     echo "OWASP ZAP did not report any Risk"
  fi;
